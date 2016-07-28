3e7c09aacf5ee78f40598412bfda72355030e02e
zzzkky <philip.20050132@163.com>
2016-07-28 19:50:19 +0800

    add response

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var token = "zzzkky";
+    var signature = req.query.signature;
+    var timestamp = req.query.timestamp;
+    var nonce = req.query.nonce;
+    var echostr = req.query.echostr;
+    var arr = [token, timestamp, nonce];
+    arr = arr.sort();
+    var content = arr[0] + arr[1] + arr[2];
+    var crypto = require('crypto');
+    var shasum = crypto.createHash('sha1');
+    shasum.update(content);
+    var d = shasum.digest('hex');
+    if (signature == d){
+        res.send(echostr);
+        res.end();
+    } else {
+        res.send("fail");
+        res.end();
+    }
+
 });
 
 module.exports = router;
